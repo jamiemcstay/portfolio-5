2c1f772b7e2bea27aca2f917136f610dcbfc632a
Jamie McStay <jamiemcstay@hotmail.com>
2025-01-29 16:45:05 +0000

    Add menu_items view, urls, and template

---
+++ menu/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.menu_items, name='menu_items'),
+]
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
+
+def menu_items(request):
+
+    """ A view to show the menu items
+    """
+
+    menu_items = MenuItem.objects.all()
+
+
+    context = {
+    'menu_items': menu_items,  # Key-value pair for the template
+    }
+
+
+    return render(request, 'menu/menu_items.html', context)
