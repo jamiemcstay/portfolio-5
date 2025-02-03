0dbd68a50ba181da3329de83aca7b8d4b167f3f6
jamiemcstay <jamiemcstay@hotmail.com>
2025-02-03 17:12:28 +0000

    Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from menu.models import MenuItem
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     item_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        menu_item = get_object_or_404(MenuItem, pk=item_id)
+        total += quantity * menu_item.price
+        item_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'menu_item': menu_item,
+        })
 
     if total > 0:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
