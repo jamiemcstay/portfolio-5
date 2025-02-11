b16b5d32538eeaf6d9d1abbfe5a07d6929e9b41f
jamiemcstay <jamiemcstay@hotmail.com>
2025-02-11 16:22:02 +0000

    Add views and js to adjust and remove items from bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 
 # Create your views here.
 
@@ -20,3 +21,32 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    if request.method == 'POST':
+        try:
+            bag = request.session.get('bag', {})
+            if item_id in bag:
+                bag.pop(item_id)
+                request.session['bag'] = bag
+                return HttpResponse(status=200)
+            else:
+                return HttpResponse(status=404)  # Item not found in the bag
+        except KeyError:
+            return HttpResponse(status=404)  # If something goes wrong
+    return HttpResponseNotAllowed("Only POST method is allowed")
