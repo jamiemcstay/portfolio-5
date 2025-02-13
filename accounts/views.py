557d568280154c9ea1fe54ae5b07b1eb462402d2
jamiemcstay <jamiemcstay@hotmail.com>
2025-02-13 17:37:25 +0000

    Fix country field text color on account page form

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def account(request):
 
+    
     account = get_object_or_404(UserAccount, user=request.user)
+    form = UserAccountForm(instance=account)
+    orders = account.orders.all()
     template = 'accounts/account.html'
     context = {
+        'form': form,
+        'orders': orders,
         'account': account,
     }
 
